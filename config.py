19fe7312be0c1016a33082fb22c9f075308e3bfa
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-07-18 00:24:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "2097755"))
+API_ID = int(getenv("API_ID", "20977551"))
 API_HASH = getenv("API_HASH", "c5c11856866072c7b12a4e32e6a0a0f9")
 BOT_TOKEN = getenv("BOT_TOKEN", "8063486217:AAHST_XwijXhNnKQMxUj0jf5jokOepcezss")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
