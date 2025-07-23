d488a49a3990b3e7d9ea012b1486969be9871fbd
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-07-24 02:56:29 +0530

    Update bot.py

--- EsproMusic/core/bot.py
+++ EsproMusic/core/bot.py
@@ -1,26 +1,7 @@
-from pyrogram import Client, errors
-from pyrogram.enums import ChatMemberStatus, ParseMode
-
-import config
-
-from ..logging import LOGGER
-
-
-class Loy(Client):
-    def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
-        super().__init__(
-            name="EsproMusic",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            parse_mode=ParseMode.HTML,
-            max_concurrent_transmissions=7,
-        )
-
-    async def start(self):
+async def start(self):
         await super().start()
+        
+        self.me = await self.get_me()  # ← This line is necessary!
         self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
@@ -49,6 +30,3 @@ class Loy(Client):
             )
             exit()
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
-
-    async def stop(self):
-        await super().stop()
